8bff0b591e4d076e7619bd1846fec760a2c9c0fe
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-05-20 23:44:47 -0300

    ProcedimentoQuantidade: Encapsulação do thrift feita

--- src/main/java/com/github/Eddyosos/integracao20171/esus/cds/atendimentoodontologico/ProcedimentoQuantidade.java
+++ src/main/java/com/github/Eddyosos/integracao20171/esus/cds/atendimentoodontologico/ProcedimentoQuantidade.java
@@ -1,6 +1,86 @@
 package com.github.Eddyosos.integracao20171.esus.cds.atendimentoodontologico;
 
-public class ProcedimentoQuantidade {
+import br.gov.saude.esus.cds.transport.generated.thrift.atendimentoodontologico.ProcedimentoQuantidadeThrift;
+import org.apache.thrift.TException;
 
+public class ProcedimentoQuantidade {
+    ProcedimentoQuantidadeThrift procedimentoQuantidadeThrift = new ProcedimentoQuantidadeThrift();
+    
+    protected ProcedimentoQuantidade(ProcedimentoQuantidadeThrift t){
+        this.procedimentoQuantidadeThrift = t;
+    }
+
+    public ProcedimentoQuantidadeThrift deepCopy() {
+        return procedimentoQuantidadeThrift.deepCopy();
+    }
+
+    public String getCoMsProcedimento() {
+        return procedimentoQuantidadeThrift.getCoMsProcedimento();
+    }
+
+    public void setCoMsProcedimento(String coMsProcedimento) {
+        procedimentoQuantidadeThrift.setCoMsProcedimento(coMsProcedimento);
+    }
+
+    public void unsetCoMsProcedimento() {
+        procedimentoQuantidadeThrift.unsetCoMsProcedimento();
+    }
+
+    public boolean isSetCoMsProcedimento() {
+        return procedimentoQuantidadeThrift.isSetCoMsProcedimento();
+    }
+
+    public void setCoMsProcedimentoIsSet(boolean value) {
+        procedimentoQuantidadeThrift.setCoMsProcedimentoIsSet(value);
+    }
+
+    public int getQuantidade() {
+        return procedimentoQuantidadeThrift.getQuantidade();
+    }
+
+    public void setQuantidade(int quantidade) {
+        procedimentoQuantidadeThrift.setQuantidade(quantidade);
+    }
+
+    public void unsetQuantidade() {
+        procedimentoQuantidadeThrift.unsetQuantidade();
+    }
+
+    public boolean isSetQuantidade() {
+        return procedimentoQuantidadeThrift.isSetQuantidade();
+    }
+
+    public void setQuantidadeIsSet(boolean value) {
+        procedimentoQuantidadeThrift.setQuantidadeIsSet(value);
+    }
+
+    public boolean equals(Object that) {
+        return procedimentoQuantidadeThrift.equals(that);
+    }
+
+    public boolean equals(ProcedimentoQuantidade that) {
+        return procedimentoQuantidadeThrift.equals(that.getInstance());
+    }
+
+    public int hashCode() {
+        return procedimentoQuantidadeThrift.hashCode();
+    }
+
+    public int compareTo(ProcedimentoQuantidade other) {
+        return procedimentoQuantidadeThrift.compareTo(other.getInstance());
+    }
+
+    public String toString() {
+        return procedimentoQuantidadeThrift.toString();
+    }
+
+    public void validate() throws TException {
+        procedimentoQuantidadeThrift.validate();
+    }
+    
+    protected ProcedimentoQuantidadeThrift getInstance(){
+        return procedimentoQuantidadeThrift;
+    }
+    
 }
 
