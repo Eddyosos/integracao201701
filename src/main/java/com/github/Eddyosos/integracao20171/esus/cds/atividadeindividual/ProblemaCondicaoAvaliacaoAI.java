971b26b18174cf9fef7c2be32ac4ea286f3a9296
Eduardo Horst <eduardoquijano2@gmail.com>
2017-05-21 21:05:54 -0300

    incomplete

--- src/main/java/com/github/Eddyosos/integracao20171/esus/cds/atividadeindividual/ProblemaCondicaoAvaliacaoAI.java
+++ src/main/java/com/github/Eddyosos/integracao20171/esus/cds/atividadeindividual/ProblemaCondicaoAvaliacaoAI.java
@@ -1,6 +1,21 @@
 package com.github.Eddyosos.integracao20171.esus.cds.atividadeindividual;
 
-public class ProblemaCondicaoAvaliacaoAI {
+import br.gov.saude.esus.cds.transport.generated.thrift.atividadeindividual.ProblemaCondicaoAvaliacaoAIThrift;
 
+public class ProblemaCondicaoAvaliacaoAI {
+    private ProblemaCondicaoAvaliacaoAIThrift instance;
+
+    public ProblemaCondicaoAvaliacaoAI() {
+        instance = new ProblemaCondicaoAvaliacaoAIThrift();
+    }
+
+    ProblemaCondicaoAvaliacaoAI(ProblemaCondicaoAvaliacaoAIThrift instance) {
+        this.instance = instance;
+    }
+    
+    ProblemaCondicaoAvaliacaoAIThrift getInstance(){
+        return instance;
+    }
+    
 }
 
