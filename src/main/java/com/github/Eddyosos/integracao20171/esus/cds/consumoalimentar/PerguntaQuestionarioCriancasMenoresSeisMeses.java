ce460d2e69130e58336109319052c5f63984e808
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-05-22 20:04:18 -0300

    Atualização dos arquivos
    Atualização dos arquivos

--- src/main/java/com/github/Eddyosos/integracao20171/esus/cds/consumoalimentar/PerguntaQuestionarioCriancasMenoresSeisMeses.java
+++ src/main/java/com/github/Eddyosos/integracao20171/esus/cds/consumoalimentar/PerguntaQuestionarioCriancasMenoresSeisMeses.java
@@ -1,6 +1,18 @@
 package com.github.Eddyosos.integracao20171.esus.cds.consumoalimentar;
 
+import br.gov.saude.esus.cds.transport.generated.thrift.consumoalimentar.PerguntaQuestionarioCriancasMenoresSeisMesesThrift;
+
 public class PerguntaQuestionarioCriancasMenoresSeisMeses {
+    PerguntaQuestionarioCriancasMenoresSeisMesesThrift instance = new PerguntaQuestionarioCriancasMenoresSeisMesesThrift();
+
+    protected PerguntaQuestionarioCriancasMenoresSeisMeses(PerguntaQuestionarioCriancasMenoresSeisMesesThrift t) {
+        this.instance = t;
+    }
+    
+    protected PerguntaQuestionarioCriancasMenoresSeisMesesThrift getInstance(){
+        return instance;
+    }
+    
 
 }
 
