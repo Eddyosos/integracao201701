06d25d3538cc6acb174558d5a99eec7cc5d78644
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-05-24 14:31:47 -0300

    Modificações finais de Encapsulamento
    Modificações finais de Encapsulamento

--- src/main/java/com/github/Eddyosos/integracao20171/esus/cds/consumoalimentar/PerguntaQuestionarioCriancasMenoresSeisMeses.java
+++ src/main/java/com/github/Eddyosos/integracao20171/esus/cds/consumoalimentar/PerguntaQuestionarioCriancasMenoresSeisMeses.java
@@ -1,6 +1,8 @@
 package com.github.Eddyosos.integracao20171.esus.cds.consumoalimentar;
 
+import br.gov.saude.esus.cds.transport.generated.thrift.consumoalimentar.PerguntaCriancasMenoresSeisMesesEnumThrift;
 import br.gov.saude.esus.cds.transport.generated.thrift.consumoalimentar.PerguntaQuestionarioCriancasMenoresSeisMesesThrift;
+import br.gov.saude.esus.cds.transport.generated.thrift.consumoalimentar.RespostaUnicaEscolhaEnumThrift;
 
 public class PerguntaQuestionarioCriancasMenoresSeisMeses {
     PerguntaQuestionarioCriancasMenoresSeisMesesThrift instance = new PerguntaQuestionarioCriancasMenoresSeisMesesThrift();
@@ -12,7 +14,41 @@ public class PerguntaQuestionarioCriancasMenoresSeisMeses {
     protected PerguntaQuestionarioCriancasMenoresSeisMesesThrift getInstance(){
         return instance;
     }
-    
 
+    public PerguntaQuestionarioCriancasMenoresSeisMeses deepCopy() {
+        return new PerguntaQuestionarioCriancasMenoresSeisMeses(instance.deepCopy());
+    }
+
+    public void unsetPergunta() {
+        instance.unsetPergunta();
+    }
+
+    public boolean isSetPergunta() {
+        return instance.isSetPergunta();
+    }
+
+    public void setPerguntaIsSet(boolean value) {
+        instance.setPerguntaIsSet(value);
+    }
+
+    public void unsetRespostaUnicaEscolha() {
+        instance.unsetRespostaUnicaEscolha();
+    }
+
+    public boolean isSetRespostaUnicaEscolha() {
+        return instance.isSetRespostaUnicaEscolha();
+    }
+
+    public void setRespostaUnicaEscolhaIsSet(boolean value) {
+        instance.setRespostaUnicaEscolhaIsSet(value);
+    }
+
+    public int compareTo(PerguntaQuestionarioCriancasMenoresSeisMeses other) {
+        return instance.compareTo(other.instance);
+    }
+
+    public boolean equals(PerguntaQuestionarioCriancasMenoresSeisMeses that) {
+        return instance.equals(that.instance);
+    }           
 }
 
