4cd16473e61e93eabf8a8fea2e8014180a2a87bc
Eduardo Horst <eduardoquijano2@gmail.com>
2017-06-24 02:54:27 -0300

    HeaderCdsCadastro

--- src/main/java/com/github/Eddyosos/integracao20171/esus/cds/common/HeaderCdsCadastro.java
+++ src/main/java/com/github/Eddyosos/integracao20171/esus/cds/common/HeaderCdsCadastro.java
@@ -7,178 +7,310 @@ import org.apache.thrift.protocol.TProtocol;
 public class HeaderCdsCadastro {
     private HeaderCdsCadastroThrift instance;
 
-    
     public HeaderCdsCadastro() {
         instance = new HeaderCdsCadastroThrift();
     }
 
-    //Devia ser protected
     public HeaderCdsCadastro(HeaderCdsCadastroThrift instance) {
         this.instance = instance;
     }
     
-    //Devia ser protected
     public HeaderCdsCadastroThrift getInstance() {
         return instance;
     }
 
-    public void clear() {
-        instance.clear();
+    /**
+     * Valida contra todas as validações da classe, caso uma falhe, esta falha
+     * também
+     * @return true se válido
+     *         false se inválido
+     */
+    public boolean validates(){
+        return validateCnesUnidadeSaude() &&
+                validateCnsProfissional() &&
+                validateCodigoIbgeMunicipio() &&
+                validateDataAtendimento() &&
+                validateIneEquipe() &&
+                validateMicroarea();
     }
-
+    
+    /**
+     * Valida CnesUnidadeSaude
+     * Para ser válido, deve ter sido inserido préviamente e ter 7 caracteres.
+     * @return true se válido
+     *          false se inválido
+     */
+    public boolean validateCnesUnidadeSaude(){
+        return instance.isSetCnesUnidadeSaude() &&
+                instance.getCnesUnidadeSaude().length() == 7;
+    }
+    
+    /**
+     * CNES da unidade de saúde que o profissional está lotado.
+     * @return CNES
+     */
     public String getCnesUnidadeSaude() {
         return instance.getCnesUnidadeSaude();
     }
 
+    /**
+     * CNES da unidade de saúde que o profissional está lotado.
+     * @param cnesUnidadeSaude 
+     */
     public void setCnesUnidadeSaude(String cnesUnidadeSaude) {
         instance.setCnesUnidadeSaude(cnesUnidadeSaude);
     }
 
-    public void unsetCnesUnidadeSaude() {
-        instance.unsetCnesUnidadeSaude();
+    /**
+     * Valida de cns
+     * Para ser válido, deve ter sido setado préviamente com uma string de 15 
+     * caracteres, ainda a mesma deve passar no algoritmo descrito em:
+     * http://cartaonet.datasus.gov.br/Rotina_Java.doc
+     * @return true se válido
+     *         false se inválido
+     */
+    public boolean validateCnsProfissional(){
+        String value = instance.getCnsProfissional();
+        if(value == null || value.length() != 15) return false;
+        switch (value.charAt(0)){
+            case '1':
+            case '2':
+                return validaCns(value);
+            case '7':
+            case '8':
+            case '9':
+                return validaCnsProv(value);
+            default:
+                return false;
+        }
     }
+    
+    /**
+     * Como descrito em:
+     * http://cartaonet.datasus.gov.br/Rotina_Java.doc
+     * @param cns
+     * @return 
+     */
+    private boolean validaCns(String cns){
+        if (cns.trim().length() != 15){
+            return(false);
+        }
 
-    public boolean isSetCnesUnidadeSaude() {
-        return instance.isSetCnesUnidadeSaude();
+        float soma;
+        float resto, dv;
+        String pis = new String("");
+        String resultado = new String("");
+        pis = cns.substring(0,11);
+
+        soma = ((Integer.valueOf(pis.substring(0,1)).intValue()) * 15) +
+               ((Integer.valueOf(pis.substring(1,2)).intValue()) * 14) +
+               ((Integer.valueOf(pis.substring(2,3)).intValue()) * 13) +
+               ((Integer.valueOf(pis.substring(3,4)).intValue()) * 12) +
+               ((Integer.valueOf(pis.substring(4,5)).intValue()) * 11) +
+               ((Integer.valueOf(pis.substring(5,6)).intValue()) * 10) +
+               ((Integer.valueOf(pis.substring(6,7)).intValue()) * 9) +
+               ((Integer.valueOf(pis.substring(7,8)).intValue()) * 8) +
+               ((Integer.valueOf(pis.substring(8,9)).intValue()) * 7) +
+               ((Integer.valueOf(pis.substring(9,10)).intValue()) * 6) +
+               ((Integer.valueOf(pis.substring(10,11)).intValue()) * 5);
+
+        resto = soma % 11;
+        dv = 11 - resto;
+
+        if (dv == 11){
+            dv = 0;
+        }
+
+        if (dv == 10){
+            soma = ((Integer.valueOf(pis.substring(0,1)).intValue()) * 15) +
+                   ((Integer.valueOf(pis.substring(1,2)).intValue()) * 14) +
+                   ((Integer.valueOf(pis.substring(2,3)).intValue()) * 13) +
+                   ((Integer.valueOf(pis.substring(3,4)).intValue()) * 12) +
+                   ((Integer.valueOf(pis.substring(4,5)).intValue()) * 11) +
+                   ((Integer.valueOf(pis.substring(5,6)).intValue()) * 10) +
+                   ((Integer.valueOf(pis.substring(6,7)).intValue()) * 9) +
+                   ((Integer.valueOf(pis.substring(7,8)).intValue()) * 8) +
+                   ((Integer.valueOf(pis.substring(8,9)).intValue()) * 7) +
+                   ((Integer.valueOf(pis.substring(9,10)).intValue()) * 6) +
+                   ((Integer.valueOf(pis.substring(10,11)).intValue()) * 5) + 2;
+
+            resto = soma % 11;
+            dv = 11 - resto;
+            resultado = pis + "001" + String.valueOf((int)dv);
+        }else {
+            resultado = pis + "000" + String.valueOf((int)dv);
+        }
+
+        if (! cns.equals(resultado)){
+            return(false);
+        }else {
+        return(true);}
     }
+    
+    /**
+     * Como descrito em:
+     * http://cartaonet.datasus.gov.br/Rotina_Java.doc
+     * @param cns
+     * @return 
+     */
+    private boolean validaCnsProv(String cns){
+        if (cns.trim().length() != 15){
+            return(false);
+        }
 
-    public void setCnesUnidadeSaudeIsSet(boolean value) {
-        instance.setCnesUnidadeSaudeIsSet(value);
+        float dv;
+        float resto,soma;
+
+        soma = ((Integer.valueOf(cns.substring(0,1)).intValue()) * 15) +
+               ((Integer.valueOf(cns.substring(1,2)).intValue()) * 14) +
+               ((Integer.valueOf(cns.substring(2,3)).intValue()) * 13) +
+               ((Integer.valueOf(cns.substring(3,4)).intValue()) * 12) +
+               ((Integer.valueOf(cns.substring(4,5)).intValue()) * 11) +
+               ((Integer.valueOf(cns.substring(5,6)).intValue()) * 10) +
+               ((Integer.valueOf(cns.substring(6,7)).intValue()) * 9) +
+               ((Integer.valueOf(cns.substring(7,8)).intValue()) * 8) +
+               ((Integer.valueOf(cns.substring(8,9)).intValue()) * 7) +
+               ((Integer.valueOf(cns.substring(9,10)).intValue()) * 6) +
+               ((Integer.valueOf(cns.substring(10,11)).intValue()) * 5) +
+               ((Integer.valueOf(cns.substring(11,12)).intValue()) * 4) +
+               ((Integer.valueOf(cns.substring(12,13)).intValue()) * 3) +
+               ((Integer.valueOf(cns.substring(13,14)).intValue()) * 2) +
+               ((Integer.valueOf(cns.substring(14,15)).intValue()) * 1);
+
+        resto = soma % 11;
+
+        if (resto != 0){
+            return(false);
+        }else {
+        return(true);}
     }
-
+    
+    /**
+     * CNS do profissional
+     * @return CNS
+     */
     public String getCnsProfissional() {
         return instance.getCnsProfissional();
     }
 
+    /**
+     * CNS do profissional
+     * @param cnsProfissional 
+     */
     public void setCnsProfissional(String cnsProfissional) {
         instance.setCnsProfissional(cnsProfissional);
     }
 
-    public void unsetCnsProfissional() {
-        instance.unsetCnsProfissional();
+    
+    /**
+     * Valida Código IBGE do município.
+     * Para ser válido, a propriedade deve ter sido atribuida préviamente e deve
+     * obedecer ao seguinte regex: "\A\d{7}\z"
+     * @return true se válido
+     *         false se inválido
+     */
+    public boolean validateCodigoIbgeMunicipio(){
+        String var = instance.getCodigoIbgeMunicipio(); 
+        return var != null &&
+               var.matches("\\A\\d{7}\\z");
     }
-
-    public boolean isSetCnsProfissional() {
-        return instance.isSetCnsProfissional();
-    }
-
-    public void setCnsProfissionalIsSet(boolean value) {
-        instance.setCnsProfissionalIsSet(value);
-    }
-
+    
+    /**
+     * Código IBGE do município.
+     * @return 
+     */
     public String getCodigoIbgeMunicipio() {
         return instance.getCodigoIbgeMunicipio();
     }
 
+    /**
+     * Código IBGE do município.
+     * @param codigoIbgeMunicipio 
+     */
     public void setCodigoIbgeMunicipio(String codigoIbgeMunicipio) {
         instance.setCodigoIbgeMunicipio(codigoIbgeMunicipio);
     }
 
-    public void unsetCodigoIbgeMunicipio() {
-        instance.unsetCodigoIbgeMunicipio();
+    /**
+     * Valida Data de atendimento
+     * para ser válida, deve ter sido inserida préviamente
+     * @return true se válido
+     *          false se inválido
+     */
+    public boolean validateDataAtendimento(){
+        return instance.isSetDataAtendimento();
     }
-
-    public boolean isSetCodigoIbgeMunicipio() {
-        return instance.isSetCodigoIbgeMunicipio();
-    }
-
-    public void setCodigoIbgeMunicipioIsSet(boolean value) {
-        instance.setCodigoIbgeMunicipioIsSet(value);
-    }
-
+    
+    /**
+     * Data em que está sendo realizada a ação.
+     * @return 
+     */
     public long getDataAtendimento() {
         return instance.getDataAtendimento();
     }
 
+    /**
+     * Data em que está sendo realizada a ação.
+     * @param dataAtendimento 
+     */
     public void setDataAtendimento(long dataAtendimento) {
         instance.setDataAtendimento(dataAtendimento);
     }
 
-    public void unsetDataAtendimento() {
-        instance.unsetDataAtendimento();
+    /**
+     * Valida IneEquipe
+     * Para ser válido, caso tenha sido inserido préviamente, o valor deve ser 
+     * uma string de tamanho 10;
+     * @return true se válido
+     *          false se inválido
+     */
+    public boolean validateIneEquipe(){
+        String value = instance.getIneEquipe();
+        return value == null || value.length() == 10;
     }
-
-    public boolean isSetDataAtendimento() {
-        return instance.isSetDataAtendimento();
-    }
-
-    public void setDataAtendimentoIsSet(boolean value) {
-        instance.setDataAtendimentoIsSet(value);
-    }
-
+    
+    /**
+     * Código do INE da equipe do profissional.
+     * @return 
+     */
     public String getIneEquipe() {
         return instance.getIneEquipe();
     }
 
+    /**
+     * Código do INE da equipe do profissional.
+     * @param ineEquipe 
+     */
     public void setIneEquipe(String ineEquipe) {
         instance.setIneEquipe(ineEquipe);
     }
 
-    public void unsetIneEquipe() {
-        instance.unsetIneEquipe();
+    /**
+     * Valida Microarea
+     * Para ser válida, se foi inserido préviamente o valor, ele deve estar entre
+     * 0 e 2 (inclusivo)
+     * @return 
+     */
+    public boolean validateMicroarea(){
+        return !instance.isSetMicroarea() || (
+                instance.getMicroarea() >= 0 &&
+                instance.getMicroarea() <= 2);
     }
-
-    public boolean isSetIneEquipe() {
-        return instance.isSetIneEquipe();
-    }
-
-    public void setIneEquipeIsSet(boolean value) {
-        instance.setIneEquipeIsSet(value);
-    }
-
+    
+    /**
+     * Microárea onde está sendo realizada a ação.
+     * @return 
+     */
     public long getMicroarea() {
         return instance.getMicroarea();
     }
 
+    /**
+     * Microárea onde está sendo realizada a ação.
+     * @param microarea 
+     */
     public void setMicroarea(long microarea) {
         instance.setMicroarea(microarea);
     }
-
-    public void unsetMicroarea() {
-        instance.unsetMicroarea();
-    }
-
-    public boolean isSetMicroarea() {
-        return instance.isSetMicroarea();
-    }
-
-    public void setMicroareaIsSet(boolean value) {
-        instance.setMicroareaIsSet(value);
-    }
-
-    public boolean equals(Object that) {
-        return instance.equals(that);
-    }
-
-    public boolean equals(HeaderCdsCadastro that) {
-        return instance.equals(that.instance);
-    }
-
-    public int hashCode() {
-        return instance.hashCode();
-    }
-
-    public int compareTo(HeaderCdsCadastro other) {
-        return instance.compareTo(other.instance);
-    }
-
-    public void read(TProtocol iprot) throws TException {
-        instance.read(iprot);
-    }
-
-    public void write(TProtocol oprot) throws TException {
-        instance.write(oprot);
-    }
-
-    public String toString() {
-        return instance.toString();
-    }
-
-    public void validate() throws TException {
-        instance.validate();
-    }
-    
-    
-    
 }
 
---
+++ src/test/java/com/github/Eddyosos/integracao20171/esus/cds/common/HeaderCdsCadastroTest.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.Eddyosos.integracao20171.esus.cds.common;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author eddyosos
+ */
+public class HeaderCdsCadastroTest {
+    
+    /**
+     * Valida cnesUnidadeSaude para casos positivos
+     */
+    @Test
+    public void testValidCnesUnidadeSaude(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        obj.setCnesUnidadeSaude("1234567");
+        assertTrue(obj.validateCnesUnidadeSaude());
+    }
+    
+    /**
+     * Valida cnesUnidadeSaude para casos negativos
+     */
+    @Test
+    public void testInvalidCnesUnidadeSaude(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        assertFalse(obj.validateCnesUnidadeSaude());
+        obj.setCnesUnidadeSaude("123456");
+        assertFalse(obj.validateCnesUnidadeSaude());
+    }
+    
+    /**
+     * Valida cnsProfissional para casos positivos
+     */
+    @Test
+    public void testValidCnsProfissional(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        obj.setCnsProfissional("951012110430003");
+        assertTrue(obj.validateCnsProfissional());
+        obj.setCnsProfissional("282651133700006");
+        assertTrue(obj.validateCnsProfissional());
+    }
+    
+    /**
+     * Valida cnsProfissional para casos negativos
+     */
+    @Test
+    public void testInvalidCnsProfissional(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        assertFalse(obj.validateCnsProfissional());
+        obj.setCnsProfissional("123456789012345");
+        assertFalse(obj.validateCnsProfissional());
+    }
+    
+    /**
+     * Valida Código IBGE para entradas positivas
+     */
+    @Test
+    public void testValidCodigoIBGE(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        obj.setCodigoIbgeMunicipio("4120655");
+        assertTrue(obj.validateCodigoIbgeMunicipio());
+        obj.setCodigoIbgeMunicipio("1234567");
+        assertTrue(obj.validateCodigoIbgeMunicipio());
+    }
+    
+    /**
+     * Valida Código IBGE para entradas negativas
+     */
+    @Test
+    public void testInvalidCodigoIBGE(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        assertFalse(obj.validateCodigoIbgeMunicipio());
+        obj.setCodigoIbgeMunicipio("12345678");
+        assertFalse(obj.validateCodigoIbgeMunicipio());
+        obj.setCodigoIbgeMunicipio("123456");
+        assertFalse(obj.validateCodigoIbgeMunicipio());
+    }
+    
+    /**
+     * Valida DataAtendimento para entradas positivas
+     */
+    @Test
+    public void testValidDataAtendimento(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        obj.setDataAtendimento(123456);
+        assertTrue(obj.validateDataAtendimento());
+    }
+    
+    /**
+     * Valida DataAtendimento para entradas negativas
+     */
+    @Test
+    public void testInvalidDataAtendimento(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        assertFalse(obj.validateCodigoIbgeMunicipio());
+    }
+    
+    /**
+     * Valida IneEquipe para entradas positivas
+     */
+    @Test
+    public void testValidIneEquipe(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        assertTrue(obj.validateIneEquipe());
+        obj.setIneEquipe("1234567890");
+        assertTrue(obj.validateIneEquipe());
+
+    }
+    
+    /**
+     * Valida IneEquipe para entradas negativas
+     */
+    @Test
+    public void testInvalidIneEquipe(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        obj.setIneEquipe("12345678");
+        assertFalse(obj.validateIneEquipe());
+    }
+    
+    /**
+     * Valida Microarea para entradas positivas
+     */
+    @Test
+    public void testValidMicroarea(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        assertTrue(obj.validateMicroarea());
+        obj.setMicroarea(1);
+        assertTrue(obj.validateMicroarea());
+
+    }
+    
+    /**
+     * Valida Microarea para entradas negativas
+     */
+    @Test
+    public void testInvalidMicroarea(){
+        HeaderCdsCadastro obj = new HeaderCdsCadastro();
+        obj.setMicroarea(125);
+        assertFalse(obj.validateMicroarea());
+    }
+}
