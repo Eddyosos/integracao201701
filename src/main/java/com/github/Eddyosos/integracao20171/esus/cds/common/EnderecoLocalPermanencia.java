b48cbf344d506c628a99150a56c156f0cb8f504b
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-06-25 01:14:18 -0300

    Validação em EnderecoLocalPermanencia
    Validação em EnderecoLocalPermanencia
    Faz parte de Ficha Cadastro Domiciliar

--- src/main/java/com/github/Eddyosos/integracao20171/esus/cds/common/EnderecoLocalPermanencia.java
+++ src/main/java/com/github/Eddyosos/integracao20171/esus/cds/common/EnderecoLocalPermanencia.java
@@ -1,371 +1,422 @@
 package com.github.Eddyosos.integracao20171.esus.cds.common;
 
 import br.gov.saude.esus.cds.transport.generated.thrift.common.EnderecoLocalPermanenciaThrift;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TProtocol;
 
-/**
- * Endereço de local de permanencia,
- * Representa local de permanencia, encapsula no padrão delegate 
- * a classe EnderecoLocalPermanenciaThrift
- */
 public class EnderecoLocalPermanencia {
-    /**
-     * Instancia thrift sendo encapsulada (padrão delegate)
-     */
-    private final EnderecoLocalPermanenciaThrift instance;
+    private EnderecoLocalPermanenciaThrift instance;
 
-    /**
-     * Construtor 
-     * Cria um objeto Thrift que será encapsulado
-     */
     public EnderecoLocalPermanencia() {
         instance = new EnderecoLocalPermanenciaThrift();
     }
     
-    /**
-     * Construtor
-     * Encapsula o objeto thrift instance
-     * @param instance a ser incapsulado
-     */
+    //Devia ser protected
     public EnderecoLocalPermanencia(EnderecoLocalPermanenciaThrift instance) {
         this.instance = instance;
     }
 
-    /**
-     * Retorna o objeto incapsulado thrift
-     * @return 
-     */
+
+    //Devia ser protected
     public EnderecoLocalPermanenciaThrift getInstance() {
         return instance;
     }
-    
-    /**
-     * Valida a classe inteira
-     * Chama todos os métodos validates, e se todos estiverem válidos, 
-     * este também retorna válido
-     * @return true se em estado válido
-     *         false se em estado inválido
-     */
-    public boolean validates(){
-        return validateBairro() &&
-                validateCEP() && 
-                validateCodigoIbgeMunicipio() && 
-                validateComplemento() &&
-                validateNomeLogradouro() &&
-                validateNumero() &&
-                validateNumeroDneUf() &&
-                validateTelReferencial() &&
-                validateTelResidencial() && 
-                validateTipoLogradouroNumeroDne();
+
+    public void clear() {
+        instance.clear();
     }
-    
-    /**
-     * Valida bairro
-     * Para ser válido ele deve ter sido atribuido anteriormente para um texto de
-     * 1 a 256 (inclusivo) caracteres.
-     * @return true se bairro for válido
-     *         false se bairro for inválido
-     */
-    public boolean validateBairro() {
-        String var = instance.getBairro();
-        return  var != null && 
-                var.length() >= 1 && 
-                var.length() <= 256;
-    }
-    
-    /**
-     * Bairro onde está localizado o domicílio
-     * @return nome do bairro
-     */
+
     public String getBairro() {
         return instance.getBairro();
     }
 
-    /**
-     * Insere o nome do bairro
-     * @param bairro nome do bairro
-     */
     public void setBairro(String bairro) {
         instance.setBairro(bairro);
     }
 
-    /**
-     * Valida o CEP.
-     * Para ser válido, o CEP deve ter sido atribuido anteriormente e deve obedecer
-     * ao seguinte regex: \A\d{8}\z (8 números)
-     * @return true se CEP for valido
-     *         false se CEP for inválido
-     */
-    public boolean validateCEP(){
-        String var = instance.getCep();
-        return var != null && var.matches("\\A\\d{8}\\z");
+    public void unsetBairro() {
+        instance.unsetBairro();
     }
-    /**
-     * CEP do logradouro do domicílio.
-     * @return Cep em string
-     */
+
+    public boolean isSetBairro() {
+        return instance.isSetBairro();
+    }
+
+    public void setBairroIsSet(boolean value) {
+        instance.setBairroIsSet(value);
+    }
+
     public String getCep() {
         return instance.getCep();
     }
 
-    /**
-     * CEP do logradouro do domicílio.
-     * @param cep 
-     */
     public void setCep(String cep) {
         instance.setCep(cep);
     }
 
-    /**
-     * Valida Código IBGE do município.
-     * Para ser válido, a propriedade deve ter sido atribuida préviamente e deve
-     * obedecer ao seguinte regex: "\A\d{7}\z"
-     * @return true se válido
-     *         false se inválido
-     */
-    public boolean validateCodigoIbgeMunicipio(){
-        String var = instance.getCodigoIbgeMunicipio(); 
-        return var != null &&
-               var.matches("\\A\\d{7}\\z");
+    public void unsetCep() {
+        instance.unsetCep();
     }
-    
-    /**
-     * Código IBGE do município.
-     * @return código
-     */
+
+    public boolean isSetCep() {
+        return instance.isSetCep();
+    }
+
+    public void setCepIsSet(boolean value) {
+        instance.setCepIsSet(value);
+    }
+
     public String getCodigoIbgeMunicipio() {
         return instance.getCodigoIbgeMunicipio();
     }
 
-    /**
-     * Código IBGE do município
-     * @param codigoIbgeMunicipio o código 
-     */
     public void setCodigoIbgeMunicipio(String codigoIbgeMunicipio) {
         instance.setCodigoIbgeMunicipio(codigoIbgeMunicipio);
     }
 
-    /**
-     * Valida o complemento.
-     * Para ser válido, se tiver sido préviamente atribuido, deve ter comprimento
-     * entre 1 e 256 caracteres (inclusivo)
-     * @return true se válido
-     *         false se inválido
-     */
-    public boolean validateComplemento(){
-        String var = instance.getComplemento();
-        return var == null ||
-                (var.length() >= 1 && var.length() <= 256);
+    public void unsetCodigoIbgeMunicipio() {
+        instance.unsetCodigoIbgeMunicipio();
     }
-    
-    /**
-     * Complemento do endereço do domicílio.
-     * @return o complemento
-     */
+
+    public boolean isSetCodigoIbgeMunicipio() {
+        return instance.isSetCodigoIbgeMunicipio();
+    }
+
+    public void setCodigoIbgeMunicipioIsSet(boolean value) {
+        instance.setCodigoIbgeMunicipioIsSet(value);
+    }
+
     public String getComplemento() {
         return instance.getComplemento();
     }
 
-    /**
-     * Complemento do endereço do domicílio.
-     * @param complemento
-     * 
-     */
     public void setComplemento(String complemento) {
         instance.setComplemento(complemento);
     }
-    
-    /**
-     * Válido o Nome do logradouro
-     * para ser válido, o nome deve ter sido inserido préviamente e deve conter
-     * entre 1 e 256 caracteres (inclusivo)
-     * @return true se válido
-     *         false se inválido
-     */
-    public boolean validateNomeLogradouro(){
-        String var = instance.getNomeLogradouro();
-        return var != null &&
-               var.length() >= 1 && var.length() <= 256;
+
+    public void unsetComplemento() {
+        instance.unsetComplemento();
     }
-    
-    /**
-     * Nome do logradouro.
-     * @return nome
-     */
+
+    public boolean isSetComplemento() {
+        return instance.isSetComplemento();
+    }
+
+    public void setComplementoIsSet(boolean value) {
+        instance.setComplementoIsSet(value);
+    }
+
     public String getNomeLogradouro() {
         return instance.getNomeLogradouro();
     }
 
-    /**
-     * Nome do logradouro.
-     * @param nomeLogradouro Nome do logradouro
-     */
     public void setNomeLogradouro(String nomeLogradouro) {
         instance.setNomeLogradouro(nomeLogradouro);
     }
-    
-    /**
-     * Número do domicílio.
-     * Para ser válido, deve ter sido inserida previamente (a não ser que,
-     * stSemNumero = true) e obedecer ao regex: \A\d{1,10}\z
-     * 
-     * @return true se válido
-     *         false se inválido
-     */
-    public boolean validateNumero(){
-        String var = instance.getNumero();
-        return (instance.isStSemNumero() == (var == null)) && 
-               (instance.isStSemNumero() || var.matches("\\A\\d{1,10}\\z"));
+
+    public void unsetNomeLogradouro() {
+        instance.unsetNomeLogradouro();
     }
 
-    /**
-     * Número do domicílio.
-     * @return número
-     */
+    public boolean isSetNomeLogradouro() {
+        return instance.isSetNomeLogradouro();
+    }
+
+    public void setNomeLogradouroIsSet(boolean value) {
+        instance.setNomeLogradouroIsSet(value);
+    }
+
     public String getNumero() {
         return instance.getNumero();
     }
 
-    /**
-     * Número do domicílio.
-     * @param numero 
-     */
     public void setNumero(String numero) {
         instance.setNumero(numero);
     }
 
-    /**
-     * Valida NumeroDneUf
-     * Para ser válido deve ter sido préviamente inserido e corresponder ao
-     * regex: "\A\d{2}\z"
-     * @return true se válido
-     *         false se inválido
-     */
-    public boolean validateNumeroDneUf() {
-        String var = instance.getNumeroDneUf();
-        return var != null && var.matches("\\A\\d{2}\\z");
+    public void unsetNumero() {
+        instance.unsetNumero();
     }
-    
-    /**
-     * Código indexador referente a Unidade Federativa.
-     * @return código
-     */
+
+    public boolean isSetNumero() {
+        return instance.isSetNumero();
+    }
+
+    public void setNumeroIsSet(boolean value) {
+        instance.setNumeroIsSet(value);
+    }
+
     public String getNumeroDneUf() {
         return instance.getNumeroDneUf();
     }
 
-    /**
-     * Código indexador referente a Unidade Federativa.
-     * @param numeroDneUf 
-     */
     public void setNumeroDneUf(String numeroDneUf) {
         instance.setNumeroDneUf(numeroDneUf);
     }
-    
-    /**
-     * Valida TelReferencial
-     * Para ser válido deve, não ter sido inserido, ou, atender ao regex: 
-     * "\A\d{10,11}\z"
-     * @return 
-     */
-    public boolean validateTelReferencial(){
-        String var = instance.getTelReferencial();
-        return var == null || var.matches("\\A\\d{10,11}\\z");
+
+    public void unsetNumeroDneUf() {
+        instance.unsetNumeroDneUf();
     }
-    
-    /**
-     * Telefone de referência.
-     * @return telefone
-     */
+
+    public boolean isSetNumeroDneUf() {
+        return instance.isSetNumeroDneUf();
+    }
+
+    public void setNumeroDneUfIsSet(boolean value) {
+        instance.setNumeroDneUfIsSet(value);
+    }
+
     public String getTelReferencial() {
         return instance.getTelReferencial();
     }
 
-    /**
-     * Telefone de referência.
-     * @param telReferencial 
-     */
     public void setTelReferencial(String telReferencial) {
         instance.setTelReferencial(telReferencial);
     }
 
-    /**
-     * Valida TelResidencial
-     * Para ser válido deve, não ter sido inserido, ou, atender ao regex: 
-     * "\A\d{10,11}\z"
-     * @return 
-     */
-    public boolean validateTelResidencial(){
-        String var = instance.getTelResidencial();
-        return var == null || var.matches("\\A\\d{10,11}\\z");
+    public void unsetTelReferencial() {
+        instance.unsetTelReferencial();
     }
-    
-    /**
-     * Telefone residencial.
-     * @return 
-     */
+
+    public boolean isSetTelReferencial() {
+        return instance.isSetTelReferencial();
+    }
+
+    public void setTelReferencialIsSet(boolean value) {
+        instance.setTelReferencialIsSet(value);
+    }
+
     public String getTelResidencial() {
         return instance.getTelResidencial();
     }
 
-    /**
-     * Telefone residencial.
-     * @param telResidencial 
-     */
     public void setTelResidencial(String telResidencial) {
         instance.setTelResidencial(telResidencial);
     }
-    
-    /**
-     * Valida TipoLogradouroNumeroDne
-     * Para ser válido, deve ter sido setado préviamente
-     * @return 
-     */
-    public boolean validateTipoLogradouroNumeroDne(){
-        String var = instance.getTipoLogradouroNumeroDne();
-        return var != null && var.matches("\\A\\d{3}\\z") && 
-                instance.getNumeroDneUf() != null;
-    } 
-    
-    /**
-     * Tipo do logradouro onde está o domicílio.
-     * @return 
-     */
+
+    public void unsetTelResidencial() {
+        instance.unsetTelResidencial();
+    }
+
+    public boolean isSetTelResidencial() {
+        return instance.isSetTelResidencial();
+    }
+
+    public void setTelResidencialIsSet(boolean value) {
+        instance.setTelResidencialIsSet(value);
+    }
+
     public String getTipoLogradouroNumeroDne() {
         return instance.getTipoLogradouroNumeroDne();
     }
 
-    /**
-     * Tipo do logradouro onde está o domicílio.
-     * @param tipoLogradouroNumeroDne
-     */
     public void setTipoLogradouroNumeroDne(String tipoLogradouroNumeroDne) {
         instance.setTipoLogradouroNumeroDne(tipoLogradouroNumeroDne);
     }
 
-    // Não á regras para validate isStSemNumero, por isso método não foi criado
-    
-    /**
-     * Marcador que indica que o domicílio não possui número.
-     * @return 
-     */
+    public void unsetTipoLogradouroNumeroDne() {
+        instance.unsetTipoLogradouroNumeroDne();
+    }
+
+    public boolean isSetTipoLogradouroNumeroDne() {
+        return instance.isSetTipoLogradouroNumeroDne();
+    }
+
+    public void setTipoLogradouroNumeroDneIsSet(boolean value) {
+        instance.setTipoLogradouroNumeroDneIsSet(value);
+    }
+
     public boolean isStSemNumero() {
         return instance.isStSemNumero();
     }
 
-    /**
-     * Marcador que indica que o domicílio não possui número.
-     * @param stSemNumero 
-     */
     public void setStSemNumero(boolean stSemNumero) {
         instance.setStSemNumero(stSemNumero);
     }
 
-    /**
-     * Marcador que indica que o domicílio não possui número.
-     * @param value 
-     */
+    public void unsetStSemNumero() {
+        instance.unsetStSemNumero();
+    }
+
+    public boolean isSetStSemNumero() {
+        return instance.isSetStSemNumero();
+    }
+
     public void setStSemNumeroIsSet(boolean value) {
         instance.setStSemNumeroIsSet(value);
     }
-}
 
+
+    public boolean equals(Object that) {
+        return instance.equals(that);
+    }
+
+    public boolean equals(EnderecoLocalPermanencia that) {
+        return instance.equals(that.instance);
+    }
+
+    public int hashCode() {
+        return instance.hashCode();
+    }
+
+    public int compareTo(EnderecoLocalPermanencia other) {
+        return instance.compareTo(other.instance);
+    }
+
+    public void read(TProtocol iprot) throws TException {
+        instance.read(iprot);
+    }
+
+    public void write(TProtocol oprot) throws TException {
+        instance.write(oprot);
+    }
+
+    public String toString() {
+        return instance.toString();
+    }
+
+    public void validate() throws TException {
+        instance.validate();
+    }
+    
+    /**
+     * Metodo validade cria os metodos que fazem as validações
+     * Chama todos os metodos que fazem validações
+     * @return Todos os metodos de validação 
+     */
+    public boolean validates(){
+        return validaBairro() &&
+                validaCep() &&
+                validaCodigoIbgeMunicipio() &&
+                validaComplemento() &&
+                validaNomeLogradouro() &&
+                validaNumero() &&
+                validaNumeroDneUF() &&
+                validaTelReferencial() &&
+                validaTelResidencial() &&
+                validaTipoLogradouroNumeroDne() &&
+                validaStSemNumero();
+    }
+    
+    /**
+     * Bairro onde está localizado o domicílio
+     * @return bairro
+     * É Obrigatório
+     */
+    public boolean validaBairro(){
+        String bairro = instance.getBairro();
+        return bairro !=null && 
+                bairro.length() >=1 &&
+                bairro.length() <=256;
+    }
+    
+    /**
+     * CEP do logradouro do domicílio.
+     * @return  cep
+     * é Obrigatório
+     */
+    public boolean validaCep(){
+        String cep = instance.getCep();
+        return  cep !=null && 
+                cep.length()==8;
+    }
+    
+    /**
+     * Código IBGE do município.
+     * @return codigoIbgeMunicipio
+     * É Obrigatório
+     */
+    public boolean validaCodigoIbgeMunicipio(){
+       String codigoIbgeMunicipio = instance.getCodigoIbgeMunicipio();
+       return codigoIbgeMunicipio != null;
+    }
+    
+    /**
+     * Complemento do endereço do domicílio
+     * @return complemento
+     */
+    public boolean validaComplemento(){
+        String complemento = instance.getComplemento();
+        return  complemento.length() >=1 &&
+                complemento.length() <=256;
+    }
+    
+    /**
+     * Nome do logradouro.
+     * @return nomeLogradouro
+     * É Obrigatório
+     */
+    public boolean validaNomeLogradouro(){
+        String nomeLogradouro = instance.getNomeLogradouro();
+        return nomeLogradouro != null &&
+                nomeLogradouro.length() >=1 &&
+                nomeLogradouro.length() <= 256;
+    }
+    
+    /**
+     * Número do domicílio
+     * @return numero
+     * É Obrigatóio
+     * Apenas letras e números.
+     * Não pode ser preenchido caso stSemNumero = true.
+     */
+    public boolean validaNumero(){
+        String numero = instance.getNumero();
+        if(instance.isSetStSemNumero()==true) 
+            return numero !=null &&
+                numero.length() >=1 &&
+                numero.length() <=10;
+        else 
+            return false;
+    }
+    
+    /**
+     * Código indexador referente a Unidade Federativa
+     * @return numeroDneUf
+     * é Obrigatório
+     */
+    public boolean validaNumeroDneUF(){
+        String numeroDneUf = instance.getNumeroDneUf();
+        return numeroDneUf != null;
+    }
+    
+    /**
+     * Telefone de referência
+     * @return telReferencial
+     * Mínimo 10 dígitos, máximo 11 (DDD + 8 ou 9)
+     */
+    public boolean validaTelReferencial(){
+        String telReferencial = instance.getTelReferencial();
+        return telReferencial.length() >=10 &&
+                telReferencial.length() <=11;
+    }
+    
+    /**
+     * Telefone residencial
+     * @return telResidencial
+     * Mínimo 10 dígitos, máximo 11 (DDD + 8 ou 9)
+     */
+    public boolean validaTelResidencial(){
+        String telResidencial = instance.getTelResidencial();
+        return telResidencial.length() >=10 &&
+                telResidencial.length() <=11;
+    }
+    
+    /**
+     * Tipo do logradouro onde está o domicílio
+     * @return tipoLogradouroNumeroDne
+     * É Obrigatório
+     */
+    public boolean validaTipoLogradouroNumeroDne(){
+        String tipoLogradouroNumeroDne = instance.getTipoLogradouroNumeroDne();
+        return tipoLogradouroNumeroDne !=null;
+    }
+    
+    /**
+     * Marcador que indica que o domicílio não possui número
+     * @return stSemNumero
+     */
+    public boolean validaStSemNumero(){
+        return instance.isSetStSemNumero();
+    }
+}
